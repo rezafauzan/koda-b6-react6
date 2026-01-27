34773025f9f2248513b3a34b07bc3dc91d54daed
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-27 17:00:47 +0700

    Feat: update document title; update article route to find by slug not id

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Article from './pages/Article.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/article/:username/:slug",
+    element: <Article />
   }
 ])
 
