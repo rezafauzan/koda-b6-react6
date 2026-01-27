9dbb207504899a5155472a318fd0cc3b28145cf7
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-27 14:46:54 +0700

    Feat: add route; add structure for main application layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <Home />
+            </main>
+            <footer></footer>
+        </>
+    )
 }
 
 export default App
